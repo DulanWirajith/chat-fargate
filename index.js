2345926a868353c1c70e3d939971d4a6654853a0
dulanwirajith <dulanwirajith1995@gmail.com>
2024-10-23 09:22:09 +0530

    Use @socket.io/redis-adapter for communication

--- index.js
+++ index.js
@@ -1,22 +1,36 @@
 // Setup basic express server
-var express = require('express');
-var app = express();
-var path = require('path');
-var server = require('http').createServer(app);
-var Presence = require('./lib/presence');
-
+const express = require('express');
+const path = require('path');
+const http = require('http');
 const { Server } = require('socket.io');
-const redisAdapter = require('socket.io-redis');
+const { createAdapter } = require('@socket.io/redis-adapter');
+const { createClient } = require('redis');
+const Presence = require('./lib/presence');
+
+const app = express();
+const server = http.createServer(app);
 
 const io = new Server(server, {
   pingTimeout: 8000,
   pingInterval: 4000
 });
 
-// Use Redis adapter
-io.adapter(redisAdapter({ host: '3.1.218.79', port: 6379 }));
+// Create Redis clients for the adapter (one for pub, one for sub)
+const pubClient = createClient({ url: 'redis://3.1.218.79:6379' });
+const subClient = pubClient.duplicate();
 
-var port = process.env.PORT || 3000;
+// Connect Redis clients
+Promise.all([pubClient.connect(), subClient.connect()])
+  .then(() => {
+    // Use the Redis adapter
+    io.adapter(createAdapter(pubClient, subClient));
+    console.log('Redis adapter connected');
+  })
+  .catch(err => {
+    console.error('Redis connection error:', err);
+  });
+
+const port = process.env.PORT || 3000;
 
 server.listen(port, function() {
   console.log('Server listening at port %d', port);
@@ -26,11 +40,11 @@ server.listen(port, function() {
 app.use(express.static(path.join(__dirname, 'public')));
 
 io.on('connection', function(socket) {
-  var addedUser = false;
+  let addedUser = false;
 
-  // when the client emits 'new message', this listens and executes
+  // When the client emits 'new message', this listens and executes
   socket.on('new message', function(data) {
-    // we tell the client to execute 'new message'
+    // We tell the client to execute 'new message'
     socket.broadcast.emit('new message', {
       username: socket.username,
       message: data
@@ -48,13 +62,13 @@ io.on('connection', function(socket) {
     });
   });
 
-  // when the client emits 'add user', this listens and executes
+  // When the client emits 'add user', this listens and executes
   socket.on('add user', function(username) {
     if (addedUser) {
       return;
     }
 
-    // we store the username in the socket session for this client
+    // We store the username in the socket session for this client
     socket.username = username;
     Presence.upsert(socket.id, {
       username: socket.username
@@ -66,7 +80,7 @@ io.on('connection', function(socket) {
         numUsers: users.length
       });
 
-      // echo globally (all clients) that a person has connected
+      // Echo globally (all clients) that a person has connected
       socket.broadcast.emit('user joined', {
         username: socket.username,
         numUsers: users.length
@@ -74,27 +88,27 @@ io.on('connection', function(socket) {
     });
   });
 
-  // when the client emits 'typing', we broadcast it to others
+  // When the client emits 'typing', we broadcast it to others
   socket.on('typing', function() {
     socket.broadcast.emit('typing', {
       username: socket.username
     });
   });
 
-  // when the client emits 'stop typing', we broadcast it to others
+  // When the client emits 'stop typing', we broadcast it to others
   socket.on('stop typing', function() {
     socket.broadcast.emit('stop typing', {
       username: socket.username
     });
   });
 
-  // when the user disconnects.. perform this
+  // When the user disconnects, perform this
   socket.on('disconnect', function() {
     if (addedUser) {
       Presence.remove(socket.id);
 
       Presence.list(function(users) {
-        // echo globally (all clients) that a person has disconnected
+        // Echo globally (all clients) that a person has disconnected
         socket.broadcast.emit('user left', {
           username: socket.username,
           numUsers: users.length
